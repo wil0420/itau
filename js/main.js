c193613e55f5ab72747b76d1aa0ce3b56abd860c
wil0420 <wil.martinez0420@gmail.com>
2021-06-15 23:54:58 -0500

    avances cambio

--- js/main.js
+++ js/main.js
@@ -10,7 +10,35 @@ function closemodalnit(){
 }
 
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == modalnit) {
       modalnit.style.display = "none";
     }
   }
+
+
+
+  function tarjetas(id){
+    var cards = document.querySelectorAll('.tarj')
+    cards.forEach(removeclass);
+
+    function removeclass(item, index) {
+        item.classList.remove("card-active");
+    }
+
+    var element = document.getElementById(id);
+    element.classList.add("card-active");
+
+
+    var infocards = document.querySelectorAll('.infocard')
+    infocards.forEach(agregarclase);
+
+    function agregarclase(item, index) {
+        item.classList.add("hide");
+    }
+
+    var elementinfo = document.getElementById(id+'-info');
+    elementinfo.classList.remove("hide");
+
+  }
+
+
